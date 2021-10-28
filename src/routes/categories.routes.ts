b56db74a5b671a3b473435dabdef091bbc5102e8
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-10-28 01:49:40 -0300

    feat: Adicionando tipagem a entrada de dados

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+import { Category } from "../model/category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
+
+  categories.push(category);
 
   return response.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.use(categoriesRoutes);
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running!");
